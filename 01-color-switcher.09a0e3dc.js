2f006f2acd89cfb9a3039c6cc4cea0f0c5d04c7e
Vitalii-Tymkiv <104509866+Vitalii-Tymkiv@users.noreply.github.com>
2022-10-04 10:53:32 +0000

    Deploying to gh-pages from @ Vitalii-Tymkiv/goit-js-hw-09@8d195aa91bbe681f9f5518f75285b2e58ba6f687 🚀

---
+++ 01-color-switcher.09a0e3dc.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},e=document.querySelector("body");t.startBtn.addEventListener("click",(function(){n=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.startBtn.disabled=!0,t.stopBtn.disabled=!1}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),git,t.startBtn.disabled=!0,t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));let n=null;t.stopBtn.disabled=!0;
+//# sourceMappingURL=01-color-switcher.09a0e3dc.js.map
